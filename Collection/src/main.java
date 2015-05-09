d6ada04f856b53a4ad57164166414f609adbe0ad
Wandol <seok45861@gmail.com>
2015-05-09 17:27:29 +0900

    ss

--- Collection/src/main.java
+++ Collection/src/main.java
@@ -9,7 +9,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		System.out.println("zzz");
 		for(int i=0;i<arr.length;i++){
 			if(i == 0){
 				tempStr +="'"+ arr[i].toString()+ "'";
